f69ad9e871f0550223e29866a3f64cdd68a8b261
limmess <limmes@yahoo.com>
2017-02-19 13:32:54 +0200

    4-Building Forms su mano atliktomis pratybomis

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
 using System.Linq;
 
 namespace Vidly.Controllers
@@ -28,6 +31,68 @@ namespace Vidly.Controllers
             return View(movies);
         }
 
+        
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm",viewModel);
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null) return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel()
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded=DateTime.Now;
+                _context.Movies.Add(movie);
+                }
+            
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+            }
+            
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException e)
+            {
+
+                Console.WriteLine(e);
+            }
+            
+           return RedirectToAction("Index", "Movies");
+        }
+
         public ActionResult Details(int id)
         {
             var movies = _context.Movies.Include(c => c.Genre).SingleOrDefault(n => n.Id == id);
@@ -37,6 +102,7 @@ namespace Vidly.Controllers
 
             return View(movies);
         }
+
 
     }
 }
--- Vidly/Models/Genre.cs
+++ Vidly/Models/Genre.cs
@@ -12,7 +12,7 @@ namespace Vidly.Models
 
         [Required]
         [StringLength(255)]
-        public string Name { get; set; }
+       public string Name { get; set; }
 
     }
 }   
