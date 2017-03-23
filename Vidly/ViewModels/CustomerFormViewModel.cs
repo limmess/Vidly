947c3b07fae2d768cf4416a1861f20786f4e6d5e
limmess <limmes@yahoo.com>
2017-03-23 20:41:52 +0200

    po 10 pamoku

--- Vidly/ViewModels/CustomerFormViewModel.cs
+++ Vidly/ViewModels/CustomerFormViewModel.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using Vidly.Models;
 
 namespace Vidly.ViewModels
@@ -10,7 +7,5 @@ namespace Vidly.ViewModels
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; }
-
-
     }
 }
